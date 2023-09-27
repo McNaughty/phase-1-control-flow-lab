844a21c92077caba147aa941aceed3a975b8aa0a
Winnie <wimacoj@gmail.com>
2023-09-27 20:33:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    return "This one is on me!";
+    //console.log("You get a free sample");
+  } else if ((ride >= 2000) && (ride <=2500)){
+        return "I will gladly take your thirty bucks.";
+    
+      } else if (ride > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+//scuberGreetingForFeet(2000);
+
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == 'NYC'){
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous' :
+      return "Thank you so much."
+      break;
+    case 'not as generous' :
+      return "Thank you."
+      break;
+    default : 
+    return "Bye."
+  }
 }
